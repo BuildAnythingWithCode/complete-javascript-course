33418f55e9faeb8e98f26f0e0ad1a545edd6b9e9
Greg Franz <168317593+BuildAnythingWithCode@users.noreply.github.com>
2026-05-31 21:41:00 +0800

    complete project

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,32 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+// btnsOpenModal.addEventListener('click', function () {
+//   modal.classList.remove('hidden');
+//   overlay.classList.remove('hidden');
+// });
+
+const open = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const hide = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener('click', open);
+}
+
+btnCloseModal.addEventListener('click', hide);
+overlay.addEventListener('click', hide);
+
+document.addEventListener('keydown', function (event) {
+  if (!modal.classList.contains('hidden') && event.key === 'Escape') hide();
+});
